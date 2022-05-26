a0d623a8fcc749b9a8d38445ef1bdc8ca0a495ce
Leonardo Rodrigues <leonardorodriguesaai@outlook.com>
2022-05-26 15:34:02 -0300

    modelando objetos javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+const jarro = {
+    cor: "azul",
+    tipo: "cabo longo",
+    bico: "pequeno",
+    altura: 35,
+    conteudo: 900,
+    material: "vidro"
+}
+
+const pet = {
+    nome: "Sadie",
+    genero: "fêmea",
+    corDoPelo: "preto",
+    raca: "labrador",
+    treinador: "Donald Johnson",
+    peso: 30,
+    porte: "grande"
+}
+
+console.log(jarro, pet)
